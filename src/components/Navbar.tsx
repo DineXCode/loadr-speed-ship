59639e75d6811431b27c1ba156059e8465f8ab42
Dine Babu <dinebabu10@gmail.com>
2025-04-09 02:33:00 +0530

    Updated button styles in Hero section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,7 +46,7 @@ const Navbar = () => {
         <div className="flex items-center">
           <a href="#hero" className="flex items-center">
             <img 
-              src="https://ibb.co/pjBMwP5y" 
+              src="https://res.cloudinary.com/djc7mipe7/image/upload/v1744146131/Logo_LOADR_nzigft.png" 
               alt="LOADR Logo" 
               className="h-10 md:h-12"
             />
